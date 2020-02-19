96966e0985007e9a2cd2aad918bc09b0b879ef65
Ichwanul Fadhli <59676433+ichwanulf99@users.noreply.github.com>
2020-02-19 13:38:56 +0700

    Updated the Atbash process
    In this recent update, I tried to simplified the codes.

--- Atbash.php
+++ Atbash.php
@@ -13,35 +13,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class Atbash {
     
-    /** Alphabets Lookup Array Variable.
-     * 
-     * This variable contains letter from 'a' to 'z'.
-     * The variable using letter as array index. 
-     * Example : index 'a' contains letter 'z'.
-     */
+    // Alphabets Lookup Array Variable.
+    // This variable contains letter from 'a' to 'z'.
+    // The variable using letter as array index. 
+    // Example : index 'a' contains letter 'z'.
     private static $alphabets_lookup = array(
-		'a' => 'z', 'b' => 'y', 'c' => 'x', 'd' => 'w', 'e' => 'v', 
-		'f' => 'u', 'g' => 't', 'h' => 's', 'i' => 'r', 'j' => 'q', 
-		'k' => 'p', 'l' => 'o', 'm' => 'n', 'n' => 'm', 'o' => 'l', 
-		'p' => 'k', 'q' => 'j', 'r' => 'i', 's' => 'h', 't' => 'g', 
-		'u' => 'f', 'v' => 'e', 'w' => 'd', 'x' => 'c', 'y' => 'b', 'z' => 'a',
-		'A' => 'Z', 'B' => 'Y', 'C' => 'X', 'D' => 'W', 'E' => 'V', 
-		'F' => 'U', 'G' => 'T', 'H' => 'S', 'I' => 'R', 'J' => 'Q', 
-		'K' => 'P', 'L' => 'O', 'M' => 'N', 'N' => 'M', 'O' => 'L', 
-		'P' => 'K', 'Q' => 'J', 'R' => 'I', 'S' => 'H', 'T' => 'G', 
-		'U' => 'F', 'V' => 'E', 'W' => 'D', 'X' => 'C', 'Y' => 'B', 'Z' => 'A'
+        'a' => 'z', 'b' => 'y', 'c' => 'x', 'd' => 'w', 'e' => 'v', 'f' => 'u', 
+        'g' => 't', 'h' => 's', 'i' => 'r', 'j' => 'q', 'k' => 'p', 'l' => 'o', 
+        'm' => 'n', 'n' => 'm', 'o' => 'l', 'p' => 'k', 'q' => 'j', 'r' => 'i', 
+        's' => 'h', 't' => 'g', 'u' => 'f', 'v' => 'e', 'w' => 'd', 'x' => 'c', 
+        'y' => 'b', 'z' => 'a'
     );
     
 	/** Atbash Cipher Function
      * 
-     * This function can act as a cipher function 
-     * and also decipher function. This function can
-     * only process a letter, if there's other character 
-     * or number, then it will not be processed, the 
-     * input will just be returned.
+     * This function can act as a cipher and decipher function. 
+     * This function can only process a letter, if there's other character 
+     * or number, then it will not be processed, the input will just be returned.
      * 
      * @param string $message
-     * The message which will be ciphered.
+     * The message.
      * 
      * @return $output
      */
@@ -49,21 +40,32 @@ class Atbash {
         // Output variable defined as an empty variable.
         $output = '';
 
+        // The message is transformed into lowercase.
+        $editedMessage = strtolower($message);
+
+        
         // The purpose of this loop is to check the letter
-        // from the $input variable, then matching it to
-        // the $alphabets_lookup variable.
-        foreach(str_split($message) as $letter){
-            // If the input is a numeric or the letter is not
-            // listed on the lookup variable, then the output
-            // will returned as the input
-            if(is_numeric($letter) || in_array($letter, self::$alphabets_lookup) !== true){
-                // The previous value appended with the parameter value.
-                $output .= $letter;
+        // from input, then matching it to $alphabets_lookup variable.
+        for($i = 0; $i < strlen($editedMessage); $i++){
+            // If the message is a numerical characters or not listed on $alphabets_lookup, then
+            // the output will just be returned as the input message.
+            if(is_numeric($editedMessage[$i]) || in_array($editedMessage[$i], self::$alphabets_lookup) !== true){
+                // Appending the output.
+                $output .= $message[$i];
             }
+            // Else, just do a normal encryption.
             else{
-                // The previous value appended with the new value
-                // from the lookup variable.
-                $output .= self::$alphabets_lookup[$letter];
+                // If the message on index $i is an uppercase letter, then the output
+                // will also be an uppercase.
+                if(ctype_upper($message[$i])){
+                    // Appending the output and convert it to uppercase.
+                    $output .= strtoupper(self::$alphabets_lookup[$editedMessage[$i]]);
+                }
+                // Else, it's just a lowercase case letter.
+                else{
+                    // Appending the output.
+                    $output .= self::$alphabets_lookup[$message[$i]];
+                }
             }
         }
         
